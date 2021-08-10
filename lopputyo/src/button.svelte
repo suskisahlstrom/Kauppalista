aff113584aae9792caeb727651af31eb9a417552
unknown <aa4252@student.jamk.fi>
2021-08-10 14:14:06 +0300

    ekat lisäykset

---
+++ lopputyo/src/button.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let type = 'button';
+    export let disabled = false;
+    export let id;
+</script>
+
+<button {type} {id} on:click>
+    <slot />
+</button>
+
---
+++ lopputyo/src/modal.svelte
@@ -0,0 +1,17 @@
+<script>
+    import button from './button.svelte';
+</script>
+
+<div class="backdrop" on:click />
+
+<div class="modal">
+    <header>
+        <slot name="header" />
+    </header>
+    <slot />
+    <footer>
+        <slot name="footer">
+            <Button on:click>Sulje</Button>
+        </slot>
+    </footer>
+</div>
